ddc78265360b1ff74367e9b2ffa466661da8269f
kimgahyung <kimgahyung@gmail.com>
2018-12-06 18:07:11 -0700

    Added entropy calculation

--- calculate_entropy.py
+++ calculate_entropy.py
@@ -3,13 +3,7 @@ import numpy
 
 #c - covariance matrix
 def calculate_entropy(c):
-    print("Before")
-    for elem in c:
-        print(elem)
     matrix = [x * 2 * math.pi * math.e for x in c]
-    print("After")
-    for elem in matrix:
-        print(elem)
     entropy = (1 / 2) * math.log2(numpy.linalg.det(matrix))
     return entropy
 
